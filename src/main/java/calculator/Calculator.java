c12214681adaba9880263535bb09ba141efa19cf
pedramkho <pe.khorsandi@gmail.com>
2021-10-26 19:57:19 -0400

    calculator class implemented. Tests passed.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,20 @@
 package calculator;
 
 public class Calculator {
-	public int add(int a, int b) {
-		return a + b;
+	public float reverse(int a) {
+//		Throws exception if division by zero happens.
+		if (a == 0)
+			throw new ArithmeticException();
+
+		return (float) (1.0 / a);
+
+	}
+
+	public float square(int a) {
+//		Throws exception if the input is negative.
+		if (a < 0)
+			throw new ArithmeticException();
+
+		return (float) Math.sqrt(a);
 	}
 }
